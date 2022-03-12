6398d289817d09ffebc7d47f202f008132b9bc94
Gabriel Carvalho Fraga <gcarvalhofraga@gmail.com>
2022-03-11 23:18:21 -0300

    Implementei as funções de cadastro e listagem geral para professores, utilizando os mesmos passos que para alunos

--- professor.c
+++ professor.c
@@ -1,61 +1,93 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "structs.h"
+#include <ctype.h>
 
-void cadastroProfessores(Pessoa *professores)
+void inserirProfessorNaLista(Pessoa **inicio, Pessoa *novoAluno)
+{
+    Pessoa *atual;
+
+    if (*inicio == NULL)
+    {
+        *inicio = novoProfessor;
+    }
+    else
+    {
+        atual = *inicio;
+
+        while (atual->prox != NULL)
+        {
+            atual = atual->prox;
+        }
+        atual->prox = novoProfessor;
+    }
+
+    novoProfessor->prox = NULL;
+}
+
+void cadastroProfessor(Pessoa **inicio)
 {
     int i, j;
-    int tam_professores = sizeof(professores) / sizeof(professores[0]);
     char continuar;
+    Pessoa *novoProfessor = (Pessoa *)malloc(sizeof(Pessoa));
 
-    for (i = tam_professores; i < 1; i++)
+    printf("-----------------------------\n");
+    printf("Nome do professor: ");
+    fgets(novoProfessor->nome, 31, stdin);
+    size_t ln = strlen(novoProfessor->nome) - 1;
+    if (novoProfessor->nome[ln] == '\n')
+        novoProfessor->nome[ln] = '\0';
+    fflush(stdin);
+
+    printf("Digite o CPF: ");
+    fgets(novoProfessor->cpf, 14, stdin);
+    ln = strlen(novoProfessor->cpf) - 1;
+    if (novoProfessor->cpf[ln] == '\n')
+        novoProfessor->cpf[ln] = '\0';
+    fflush(stdin);
+
+    printf("Matrícula do professor: ");
+    fgets(novoProfessor->matricula, 14, stdin);
+    ln = strlen(novoProfessor->matricula) - 1;
+    if (novoProfessor->matricula[ln] == '\n')
+        novoProfessor->matricula[ln] = '\0';
+    fflush(stdin);
+
+    printf("Digite o sexo: ");
+    scanf("%c", &novoProfessor->sexo);
+    fflush(stdin);
+
+    printf("Data de nascimento\n");
+
+    printf("Dia: ");
+    scanf("%d", &novoProfessor->dataNascimento.dia);
+    fflush(stdin);
+
+    printf("Mes: ");
+    scanf("%d", &novoProfessor->dataNascimento.mes);
+    fflush(stdin);
+
+    printf("Ano: ");
+    scanf("%d", &novoProfessor->dataNascimento.ano);
+    fflush(stdin);
+
+    inserirAlunoNaLista(inicio, novoProfessor);
+}
+
+void listarProfessores(Pessoa **inicio)
+{
+    Pessoa *atual = *inicio;
+    while (atual != NULL)
     {
-        printf("Nome do professor: ");
-        fgets(professores[i].nome, 31, stdin);
-        size_t ln = strlen(professores[i].nome) - 1;
-        if (professores[i].nome[ln] == '\n')
-            professores[i].nome[ln] = '\0';
-        fflush(stdin);
+        printf("\n-----------------------------\n");
+        puts(atual->nome);
+        puts(atual->matricula);
+        puts(atual->cpf);
+        printf("%c \n", atual->sexo);
+        printf("%02d / %02d / %4d", atual->dataNascimento.dia, atual->dataNascimento.mes, atual->dataNascimento.ano);
 
-        printf("Digite o CPF: ");
-        fgets(professores[i].cpf, 14, stdin);
-        ln = strlen(professores[i].cpf) - 1;
-        if (professores[i].cpf[ln] == '\n')
-            professores[i].cpf[ln] = '\0';
-        fflush(stdin);
-
-        printf("Matrícula do aluno: ");
-        fgets(professores[i].matricula, 14, stdin);
-        ln = strlen(professores[i].matricula) - 1;
-        if (professores[i].matricula[ln] == '\n')
-            professores[i].matricula[ln] = '\0';
-        fflush(stdin);
-
-        printf("Digite o sexo: ");
-        scanf("%c", &professores[i].sexo);
-        fflush(stdin);
-
-        printf("Data de nascimento\n");
-
-        printf("Dia: ");
-        scanf("%d", &professores[i].dataNascimento.dia);
-        fflush(stdin);
-
-        printf("Mês: ");
-        scanf("%d", &professores[i].dataNascimento.mes);
-        fflush(stdin);
-
-        printf("Ano: ");
-        scanf("%d", &professores[i].dataNascimento.ano);
-        fflush(stdin);
-
-        printf("-----------------------------\n");
-        printf("Deseja cadastrar outro professor(a)? (S/N)\n");
-        scanf("%c", &continuar);
-
-        if (toupper(continuar) == "N")
-        {
-            break;
-        }
+        atual = atual->prox;
     }
 }
 
