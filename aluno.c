0e42ce6b1349103b864d3f7be1cbea4edacf5df2
Gabriel Carvalho Fraga <gcarvalhofraga@gmail.com>
2022-03-18 22:30:25 -0300

    Consertei o erro de input na função de cadastro de alunos

--- aluno.c
+++ aluno.c
@@ -6,61 +6,62 @@
 
 static int matriculaAtual = 100;
 
-int gerarMatricula(){  
-  matriculaAtual++;
-  return matriculaAtual;
+int gerarMatricula()
+{
+    matriculaAtual++;
+    return matriculaAtual;
 }
 
 int cadastroAluno(Pessoa lista_aluno[], int qtd_alunos)
-{  
+{
     printf("-----------------------------\n");
     fflush(stdin);
     printf("Nome do aluno: ");
-    printf("%s", fgets(lista_aluno[qtd_alunos].nome, 31, stdin));
+    fgets(lista_aluno[qtd_alunos].nome, 31, stdin);
     size_t ln = strlen(lista_aluno[qtd_alunos].nome) - 1;
     if (lista_aluno[qtd_alunos].nome[ln] == '\n')
-        lista_aluno[qtd_alunos].nome[ln] = '\0'; 
-    
-  
+        lista_aluno[qtd_alunos].nome[ln] = '\0';
+
     printf("Digite o CPF: ");
     fgets(lista_aluno[qtd_alunos].cpf, 15, stdin);
     ln = strlen(lista_aluno[qtd_alunos].cpf) - 1;
     if (lista_aluno[qtd_alunos].cpf[ln] == '\n')
         lista_aluno[qtd_alunos].cpf[ln] = '\0';
     getchar();
-  
+
     lista_aluno[qtd_alunos].matricula = gerarMatricula();
-   
+
     printf("Digite o sexo: ");
-    scanf("%c", &lista_aluno[qtd_alunos].sexo);    
-  
+    scanf("%c", &lista_aluno[qtd_alunos].sexo);
+
     printf("Data de nascimento\n");
-  
+
     printf("Dia: ");
     fflush(stdin);
     scanf("%d", &lista_aluno[qtd_alunos].dataNascimento.dia);
-    
+
     printf("Mes: ");
     fflush(stdin);
     scanf("%d", &lista_aluno[qtd_alunos].dataNascimento.mes);
-    
+
     printf("Ano: ");
     scanf("%d", &lista_aluno[qtd_alunos].dataNascimento.ano);
+    fflush(stdin);
 
     qtd_alunos++;
-  
+
     return qtd_alunos;
 }
 
 void listarAlunos(Pessoa lista_aluno[], int qtd_alunos)
 {
     int i;
-  
-    for(i=0; i<qtd_alunos; i++)
+
+    for (i = 0; i < qtd_alunos; i++)
     {
         printf("\n-----------------------------\n");
         puts(lista_aluno[i].nome);
-        printf("%d", lista_aluno[i].matricula);
+        printf("%d \n", lista_aluno[i].matricula);
         puts(lista_aluno[i].cpf);
         printf("%c \n", lista_aluno[i].sexo);
         printf("%02d / %02d / %4d", lista_aluno[i].dataNascimento.dia, lista_aluno[i].dataNascimento.mes, lista_aluno[i].dataNascimento.ano);
--- escola.c
+++ escola.c
@@ -11,20 +11,17 @@ void main()
     Pessoa lista_professor[TAM_LISTA_PROFESSORES];
     int qtd_alunos = 0, qtd_professores = 0;
     char continuar;
+    char continuar_break = 'N';
 
     // printf("-----------------------------\n");
     // printf("       Menu principal\n");
     // printf("-----------------------------\n");
-
-    while (continuar != 'N')
-    {  
-        qtd_alunos = cadastroAluno(lista_aluno, qtd_alunos);     
+    while (toupper(continuar) != continuar_break)
+    {
+        qtd_alunos = cadastroAluno(lista_aluno, qtd_alunos);
 
         printf("Deseja cadastrar outro aluno(a)? (S/N)\n");
         scanf("%c", &continuar);
-        continuar = toupper(continuar);
-        getchar();
-       
     }
 
     listarAlunos(lista_aluno, qtd_alunos);
