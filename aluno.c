a28685956716f5658745eb7ece5451d8aa53da74
Tainan Suzarte <71236529+tainansuzarte@users.noreply.github.com>
2022-03-22 01:28:30 +0000

    Criação da função de buscar aluno, inicio da função Apagar aluno

--- aluno.c
+++ aluno.c
@@ -4,7 +4,7 @@
 #include "structs.h"
 #include <ctype.h>
 
-static int matriculaAtual = 100;
+static int matriculaAtual = 20220000;
 
 int gerarMatricula()
 {
@@ -12,7 +12,7 @@ int gerarMatricula()
     return matriculaAtual;
 }
 
-int cadastroProfessor(Pessoa lista_aluno[], int qtd_alunos)
+int cadastroAluno(Pessoa lista_aluno[], int qtd_alunos)
 {
     printf("-----------------------------\n");
     fflush(stdin);
@@ -68,6 +68,91 @@ void listarAlunos(Pessoa lista_aluno[], int qtd_alunos)
     }
 }
 
-void apagarCadastro() {}
+void apagarCadastro() {
+
+  int retorno_busca;
+  int opcao;
+  int apagar;
+
+  printf("---Modulo de Exclusão de Alunos---")
+  printf(" Deseja: 1- Buscar ou Aluno 2 - Sair : \n");
+  scanf("%d",%opcao);
+  fflush(stdin);
+  
+  if(opcao==1){
+    retorno_busca = buscaAluno();
+
+    if(retorno_busca!=0){
+      printf("-----Dados do Aluno Encontrado-----\n");
+      printf("\n-----------------------------\n");
+        puts(lista_aluno[retorno_busca].nome);
+        printf("%d \n", lista_aluno[retorno_busca].matricula);
+        puts(lista_aluno[retorno_busca].cpf);
+        printf("%c \n", lista_aluno[retorno_busca].sexo);
+        printf("%02d / %02d / %4d", lista_aluno[retorno_busca].dataNascimento.dia, lista_aluno[retorno_busca].dataNascimento.mes, lista_aluno[retorno_busca].dataNascimento.ano);
+
+      printf("/n");
+      printf("Deseja excluir o registro desse(a) aluno(a)? 1- Sim ou 2- Não: ");
+      scanf("%d",%apagar);
+      fflush(stdin);
+
+      if(apagar==1){
+        for(i=retorno_busca,i<=, i++){
+        }
+      }
+      
+    }
+
+    
+  }
+  
+
+  
+  
+}
 
 void atualizarCadastro() {}
+
+int buscaAluno(){
+
+  int opcao;
+  int buscaMatricula;
+  char buscaNome[30];
+
+  printf("Deseja Buscar o Aluno por 1-Nome ou 2- Matricula ? : ");
+  scanf(%d, &opcao);
+
+  if(opcao==1){
+    printf("Digite o nome do aluno(a): ");
+    fgets(buscaNome, 31, stdin);
+    size_t ln = strlen(buscaNome) - 1;
+      if (buscaNome == '\n')
+          buscaNome = '\0';  
+
+    for(i=0, i<=qtd_alunos, i++){
+      if(buscaNome==lista_aluno[i].nome)
+        return i;
+      else{
+        printf("Aluno não encontrado!");
+        return 0;
+      }
+    }
+  }
+
+  if(opcao==1){
+    printf("Digite a matricula do aluno(a): ")
+    scanf(%d, &buscaMatricula);
+    fflush(stdin);  
+
+    for(i=0, i<=qtd_alunos, i++){
+      if(buscaMatricula==lista_aluno[i].matricula)
+        return i;
+      else{
+        printf("Aluno não encontrado!");
+        return 0;
+      }
+    }
+  }
+  }
+  
+}
